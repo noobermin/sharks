b4ffa66d56b3e9dd0e56a49d2b266f8b2cbfbf52
Gregory K. Ngirmang <ngirmang.1@osu.edu>
2016-07-04 23:35:47 -0400

    fixed mklin.py

--- hotwater3d_tmpl/mklin.py
+++ hotwater3d_tmpl/mklin.py
@@ -6,23 +6,21 @@ Usage:
   ./mklin.py [options]
 
 Options:
-  --x-dims=XDIM -x XDIM      Set the x dimensions. [default: (0.0, 27.5e-4)]
-  --scale=SCALE -l SCALE     Set the scale length. [default: 1.5e-4]
-  --solid-dims=SDIM -s SDIM  Set the solid dimensions. [default: (17.5e-4, 27.5e-4)]
-  --solid=SDIM -S SDIM       Set the solid number density. [default: 1.0e23]
+  --x-length=L -x L       Length of x-dimension. [default: 27.5]
+  --scale=SCALE -l SCALE  Set the scale length. [default: 1.5]
+  --solid-length=L -s L   Set the solid dimensions. [default: 10]
+  --solid=N -S N          Set the solid number density. [default: 1.0e23]
 '''
 from docopt import docopt
 import numpy as np;
-from gendat import gentargetdat
+from gendat import genonescale
 opts = docopt(__doc__,help=True);
-xdim = eval(opts['--x-dims']);
-xdim = min(*xdim),max(*xdim)
+xlen=float(opts['--x-length']);
+solid_len=float(opts['--solid-length']);
 scale = float(opts['--scale']);
 n_s = float(opts['--solid']);
-sdim = eval(opts['--solid-dims']);
-sdim = min(*sdim),max(*sdim);
-print(gentargetdat(
-    sdim=sdim + (0,0,0,0,),
-    tlim=xdim + (0,0,0,0,),
-    scale=scale,
-    n_s=n_s).decode('utf-8'))
+print(genonescale(
+    xlen=xlen,
+    solid_len=solid_len,
+    expf=scale,
+    n_s=n_s).decode('utf-8'));
