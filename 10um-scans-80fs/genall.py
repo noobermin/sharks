54936b41655878016b52e2e31373645fb9b9b17c
noobermin <ngirmang.1@osu.edu>
2016-11-21 16:04:27 -0500

    adding spatial convergence

--- 10um-scans-80fs/genall.py
+++ 10um-scans-80fs/genall.py
@@ -180,10 +180,20 @@ for E in Es:
     mkpbsbase(d);
     gensim(**d);
     d.update(
+        restart=23.95,
         timestep=5e-17,
         pbsbase=d['pbsbase']+"-convg",
         description="780nm convergence test",)
     gensim(**d);
+    d.update(
+        restart=23.95,
+        timestep=5e-17,
+        pbsbase=d['pbsbase']+"-spconvg",
+        domains=64,
+        res =( 52.5*60, 30*60, 0),
+        description="spatial 780nm convergence test",)
+    gensim(**d);
+
 
 
 
