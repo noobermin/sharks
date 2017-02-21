b55d7984d7ab3cb1ae1ee8cdb15b3dd68e7888ae
Gregory K. Ngirmang <ngirmang.1@osu.edu>
2017-02-21 02:23:25 -0500

    correcting temperature

--- oneD/oned_02/mktemp.py
+++ oneD/oned_02/mktemp.py
@@ -2,11 +2,11 @@
 
 import numpy as np;
 
-x = np.linspace(0.0, 1e-3, 1002);
+x = np.linspace(0.0, 1e-1, 1002);
 solidtemp = 100e3
 @np.vectorize
 def f(x):
-    if x <= 0.485e-3 or x >= 0.515e-3:
+    if x <= 0.485e-1 or x >= 0.515e-1:
         return 1;
     else:
         return solidtemp;
