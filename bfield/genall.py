182558b957fd75113f9d2379fd646209789f8e60
Gregory K. Ngirmang <ngirmang.1@osu.edu>
2017-07-24 19:15:34 -0400

    adding better scale B movie maker

--- bfield/genall.py
+++ bfield/genall.py
@@ -10,17 +10,19 @@ from docopt import docopt
 opts=docopt(__doc__,help=True);
 from pys import sd, parse_ituple, savetxt;
 from genpbs import genpbs;
-from gensim import gensim, fromenergy,c;
+from gensim import gensim, fromenergy;
 from gendat import gendat;
 import numpy as np;
 import shutil as sh;
-
+from lspplot.physics import e0,c,mu0
 #E = 0.5e-3; # 5mJ
 I = 2.5e18;
 l = 0.78e-6
 w0=2.2e-6 / np.sqrt(2*np.log(2))
 T0=42e-15
 
+#peak B field
+BfromI = lambda i: np.sqrt(i*1e4 * 2 / c / e0)/c*1e4;
 from gencircle import mkcircle;
 d=dict(
     l=l,
@@ -72,7 +74,7 @@ d=dict(
         clim=(-1e19,1e19),
         linthresh=1e15,),
     movB=dict(
-        clim=(1e2,1e8),
+        clim=(BfromI(I)*1e-4, BfromI(I)),
         contour_lines=(1e19,1e21),
         contour_quantities=('RhoN10', 'RhoN10'),),
     #pmovies
@@ -92,8 +94,14 @@ d2 = sd(
              fp=(0.0,-5.0, 0.0))],);
 gensim(**d2);
 Is = 5*(10.0**np.arange(16,23));
-Iscan = [ sd(d2,I = i,pbsbase="circ_{:.0e}".format(i))
-          for i in Is ];
+Bs = BfromI(Is);
+Iscan = [
+    sd(d2,
+       I = i,
+       movB = sd(d2['movB'],clim=(BfromI(I)*1e-4, BfromI(I))),
+       pbsbase="circ_{:.0e}".format(i) )
+    for i,b in zip(Is,Bs) ];
+
 for di in Iscan:
     gensim(**di);
 
