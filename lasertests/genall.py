232c67964dbc69beac1954eb773572966556249d
Gregory K. Ngirmang <ngirmang.1@osu.edu>
2018-10-16 22:50:52 -0400

    try again?

--- lasertests/genall.py
+++ lasertests/genall.py
@@ -247,6 +247,9 @@ d3d_pwb = sd(
 );
 gensim(**d3d_pwb);
 
+
+
+
 d3d_pwb2 = sd(
     d3d_pwb,
     T=20e-15,
@@ -271,6 +274,38 @@ d3d_pwb2 = sd(
 gensim(**d3d_pwb2);
 
 
+d3d_pwb3 = sd(
+    d3d_pwb2,
+    T=20e-15,
+    time_bias_iterations=0,
+    nolaser=True,
+    lim =(-2, 2, -15, 15, -6, 6),
+    totaltime=110e-15,
+    res =(4*20, 30*20, 12*10),
+    planewave_boundary=dict(
+        pwblim = [-1.5e-4, 3e-4, -16e-4, 16e-4, -7e-4, 7e-4],
+        rotation=0,
+        azimuthal_angle=0,
+        pwfunc=1,
+    ),
+    freespace = dict(
+        frlim=[-2e-4,2e-4, -15e-4, 15e-4, -6e-4, 6e-4],
+        model_type='CFSPML',
+        num_of_cells = 8,),
+    conductors=[
+        sd(cond, outlet='xmin'),
+        sd(cond, outlet='xmax'),
+        sd(cond, outlet='ymax'),
+        sd(cond, outlet='ymin'),
+        sd(cond, outlet='zmax'),
+        sd(cond, outlet='zmin'),],
+    pbsbase='pwout3',
+    lsptemplate='emonly83_tmpl.lsp',
+    movE=dict(
+        clim=(1e10,EfromI(5e18))
+    ),
+);
+gensim(**d3d_pwb3);
 
 
 gensim(**sd(
