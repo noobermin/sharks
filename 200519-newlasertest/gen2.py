e31ea44b18f2f0266b0662db184c93435d722399
Gregory K. Ngirmang <ngirmang.1@osu.edu>
2020-07-02 17:31:32 -0400

    laser 85

--- 200519-newlasertest/gen2.py
+++ 200519-newlasertest/gen2.py
@@ -144,6 +144,21 @@ d3d=sd(
 );
 gensim(**d3d);
 
+d3 = sd(d,
+        pbsbase='oeog03',
+        laser_t0 = 0,
+        multilaser = [
+            dict(
+                laser_func_type = 85,
+                outlet = 'xmin',
+                laser_dir = (1,0,0),
+                lp_mode = (0, 0),
+                laser_pol = (0, 1, 0),
+                laser_pol_type = 87)],
+        w0 = 3.0*d['l']/(np.pi/2.0),
+);
+gensim(**d3);
+
 d1=sd(
     d,
     totaltime=160e-15,
