7c3fe1a659d43853b6f9563e8ac88804f7d70bdf
Gregory K. Ngirmang <ngirmang.1@osu.edu>
2020-10-21 12:07:00 -0400

    shifting a little for 2

--- 20201013tight3d/gen2.py
+++ 20201013tight3d/gen2.py
@@ -192,11 +192,10 @@ def process_d(
     #use gaussian distance
     w  = gauss_w(xmin,xr,w0,fp=d['fp']);
     print("gauss radius: {:+.2e}".format(w));
-    rl = 2*w;
+    rl = 3*w;
     xf = 0.5*(xmax-xmin);
     s  = np.sqrt(rl**2 + xf**2) - xf;
     d['timeshift'] =  -s/c*1e-9;
-    #d['timeshift'] = (t_st)*1e-9;
     print('timeshift={} vs min={}, diff={}'.format(
         d['timeshift']*1e9,Ts.max(),d['timeshift']*1e9+Ts.max()));
     Tf = tfunc(t,d['T'],phase=d['phase']);
