6ab9d6cd530dcd325361c5206a0fec4fd6c94430
Gregory K. Ngirmang <ngirmang.1@osu.edu>
2020-10-29 02:28:11 -0400

    smaller

--- 20201028pill3D/gen0.py
+++ 20201028pill3D/gen0.py
@@ -40,7 +40,7 @@ d=dict(
     tlim=( -2e-4,1.8e-4,
            -5e-4,5.0e-4,
            -5e-4,5.0e-4),
-    res = (660,660,440),
+    res = (528,528,440),
     description = "tight3d",
     #no outputs because we do restarts now!
     restarts_only = True,
