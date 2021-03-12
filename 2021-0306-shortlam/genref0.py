4197d3b56144751e918a2c08c699c82e30445b6a
Gregory K. Ngirmang <ngirmang.1@osu.edu>
2021-03-12 17:58:04 -0500

    fix

--- 2021-0306-shortlam/genref0.py
+++ 2021-0306-shortlam/genref0.py
@@ -123,13 +123,15 @@ descrfmt = 'ti:saph ref, 45 deg, I={}, rev={}'
 def mkdescr(N,I): return descrfmt.format(I,N);
 N = 1;
 I = d['I'];
-d = sd(pbsbase  = mkpbsbase(N,I),
+d = sd(d,
+       pbsbase  = mkpbsbase(N,I),
        description = mkdescr(N,I));
 gensim(**d);
 
 N  = 2;
 I  = d['I'];
-d1 = sd(pbsbase = mkpbsbase(N,I),
+d1 = sd(d,
+        pbsbase = mkpbsbase(N,I),
         description = mkdescr(N,I),
         totaltime   = 1.05e-12,
         T       = 50e-15);
