39820a42b6085e8198671caaf164bdc6fecf04c7
Gregory K. Ngirmang <ngirmang.1@osu.edu>
2021-07-12 05:40:46 -0400

    space saving

--- 20210107pill2d/gen5.0.py
+++ 20210107pill2d/gen5.0.py
@@ -50,7 +50,7 @@ d=dict(
     #misc
     lspexec='lsp-10-xy -r',
     dir=True,
-    totaltime=  1.05e-12,
+    totaltime=  0.95e-12,
     timestep = 20.0e-18,
     restart_interval=50,
     dump_restart_flag=True,
@@ -60,8 +60,8 @@ d=dict(
     ux=1.0,
     #computational division
     region_dom_split='y',
-    region_splits = [('x',2),('y',2),('z',1)],
-    domains=4*40,
+    region_splits = [('x',2),('y',1),('z',1)],
+    domains=2*40,
     #newlaser
     new_multilaser=True,
     starting_funcnum = 1,
